78c092af4bd8a1402e0fb40555ee5740594886c0
dburdzy <dburdzy@gmail.com>
2015-10-16 15:11:35 +0200

    Removing new Samm

--- parent/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-apt-get -y install openjdk-7-jre
-java -jar /opt/testapp/testApp*.jar &
--- test/testApp/src/main/java/pl/edu/agh/samm/testapp/core/Stoppable.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.samm.testapp.core;
-
-public interface Stoppable {
-
-    void stopExecution();
-
-}
